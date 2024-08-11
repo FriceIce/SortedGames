763225a7e39a6f3e670cb678e41f1e21e87bfab7
Isak Tilahun <you@example.com>
2024-08-11 21:04:16 +0200

    Updates

---
+++ assets/PrivateRoutes-BbDslvEX.js
@@ -0,0 +1,1 @@
+import{u as t,j as s,O as r,N as u}from"./index-CckqYSqj.js";const i=()=>t(e=>e.user.user)?s.jsx(r,{}):s.jsx(u,{to:"/SortedGames/sign-in"});export{i as default};
--- assets/useFetchGamesOnScroll-Ct-rGsyL.js
+++
@@ -1,1 +0,0 @@
-import{R as t}from"./index-CB2Yse53.js";const m=e=>{const[s,u]=t.useState(50);return t.useEffect(()=>(e===!0&&u(a=>a+50),()=>{}),[e]),s};export{m as u};
