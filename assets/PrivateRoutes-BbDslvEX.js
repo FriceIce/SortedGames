1e7d84e33079ffbc012fd7802c7d4d1adc12df15
Isak Tilahun <you@example.com>
2024-08-11 21:16:07 +0200

    Updates

--- assets/PrivateRoutes-BbDslvEX.js
+++
@@ -1,1 +0,0 @@
-import{u as t,j as s,O as r,N as u}from"./index-CckqYSqj.js";const i=()=>t(e=>e.user.user)?s.jsx(r,{}):s.jsx(u,{to:"/SortedGames/sign-in"});export{i as default};
---
+++ assets/PrivateRoutes-C7o_-4vR.js
@@ -0,0 +1,1 @@
+import{u as t,j as s,O as r,N as u}from"./index-BAu8pSpB.js";const i=()=>t(e=>e.user.user)?s.jsx(r,{}):s.jsx(u,{to:"/SortedGames/sign-in"});export{i as default};
