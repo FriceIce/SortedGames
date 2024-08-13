f386f41b39ea29296f92b42e774c90e0a4ee7ca0
Isak Tilahun <you@example.com>
2024-08-13 20:22:38 +0200

    Implemented React query for fetching avatars in dashboard component.

--- src/Components/Avatar.tsx
+++ src/Components/Avatar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
 import { useSelector } from "react-redux";
 import { AvatarWithBackground } from "../definitions";
 import { updateAvatar } from "../firebase/firebase";
@@ -7,13 +7,20 @@ import { RootState } from "../redux/store";
 
 const Avatar = () => {
   const { user } = useSelector((state: RootState) => state.user);
-  const [data, setData] = React.useState<AvatarWithBackground>({
-    withBackground: [],
+  // const [data, setData] = React.useState<AvatarWithBackground>({
+  //   withBackground: [],
+  // });
+
+  const { data, isLoading } = useQuery<AvatarWithBackground>({
+    queryKey: ["Avatars"],
+    queryFn: () =>
+      fetchGames("/SortedGames/database/avatars/avatars.json", null),
   });
 
-  React.useEffect(() => {
-    fetchGames("/SortedGames/database/avatars/avatars.json", null, setData);
-  }, []);
+  // ANVÄND REACT QUERY
+  // React.useEffect(() => {
+  //   fetchGames("/SortedGames/database/avatars/avatars.json", null, setData);
+  // }, []);
 
   const setProfileImg = (avatarSrc: string | null) => {
     if (!user || !avatarSrc) return;
@@ -26,29 +33,33 @@ const Avatar = () => {
           <h1 className="text-2xl font-bold text-center">Pick your avatar</h1>
           <div className="flex justify-center">
             <ul className="w-full avatar-grid px-3 py-3">
-              {data.withBackground.map((avatar) => {
-                const currentAvatar =
-                  avatar.src === user.profileImg && avatar.src; // returns true or false
-                // if(currentAvatar) setProfileImg(currentAvatar);
-                return (
-                  <li
-                    key={avatar.id}
-                    className={`flex-1 flex flex-col gap-2 items-center text-nowrap p-2 border-2 rounded-xl transition-all cursor-pointer hover:border-[#7d61ec] ${
-                      currentAvatar ? "border-[#7d61ec]" : "border-transparent"
-                    }`}
-                    onClick={() => setProfileImg(avatar.src)}
-                  >
-                    <div className="size-[56px]">
-                      <img
-                        src={avatar.src}
-                        alt={`${avatar.name} icon`}
-                        className="size-[56px]"
-                      />
-                    </div>
-                    <p className="text-xs">{avatar.name}</p>
-                  </li>
-                );
-              })}
+              {!isLoading &&
+                data &&
+                data.withBackground.map((avatar) => {
+                  const currentAvatar =
+                    avatar.src === user.profileImg && avatar.src; // returns true or false
+                  // if(currentAvatar) setProfileImg(currentAvatar);
+                  return (
+                    <li
+                      key={avatar.id}
+                      className={`flex-1 flex flex-col gap-2 items-center text-nowrap p-2 border-2 rounded-xl transition-all cursor-pointer hover:border-[#7d61ec] ${
+                        currentAvatar
+                          ? "border-[#7d61ec]"
+                          : "border-transparent"
+                      }`}
+                      onClick={() => setProfileImg(avatar.src)}
+                    >
+                      <div className="size-[56px]">
+                        <img
+                          src={avatar.src}
+                          alt={`${avatar.name} icon`}
+                          className="size-[56px]"
+                        />
+                      </div>
+                      <p className="text-xs">{avatar.name}</p>
+                    </li>
+                  );
+                })}
             </ul>
           </div>
         </>
