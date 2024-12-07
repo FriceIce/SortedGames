6617b7d5fc3d5cbbeb03caf112a2db575410bb7f
Isak Tilahun <tilahun.isak@gmail.com>
2024-12-07 21:48:51 +0100

    .

--- server/resource/games/games.router.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getRecommendations } from "./games.controller";
-
-const router = express.Router();
-
-router.get("/getRecommendations/:id", getRecommendations);
-
-export default router;
